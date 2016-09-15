ce8cfb81ca3abb46b080cb0acee8db13f07dda11
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2016-09-15 02:38:59 +0200

    add comments

--- updateRoundcube.sh
+++ updateRoundcube.sh
@@ -6,17 +6,24 @@ APPPATH="/var/www/clickpress/apps/"
 RCPATH="${APPPATH}roundcube"
 USRGRP="clickpress.clickpress"
 
-#GET VERSIONNUMBER
+
+# GET VERSIONNUMBER
 VERSION=`curl -s https://api.github.com/repos/roundcube/roundcubemail/releases/latest | grep 'tag_name' | cut -d\" -f4`
 
-cd ${APPPATH}
+# GET PACKAGE
 wget https://github.com/roundcube/roundcubemail/releases/download/${VERSION}/roundcubemail-${VERSION}.tar.gz
 tar xvfz roundcubemail-${VERSION}.tar.gz
 cd roundcubemail-${VERSION}
+
+# GET DEPS
 cp composer.json-dist composer.json
 composer install --no-dev
+
+# INSTALL TO ORIGINAL 
 ./bin/installto.sh $RCPATH
+
+#RIGHTS
 chown -R $USRGRP $RCPATH
-cd ..
-rm -R ${APPPATH}roundcubemail-${VERSION}
-rm -R ${APPPATH}roundcubemail-${VERSION}.tar.gz
+
+#CLEAN UP
+cd .. && rm -R ${APPPATH}roundcubemail-${VERSION} &&rm -R ${APPPATH}roundcubemail-${VERSION}.tar.gz
