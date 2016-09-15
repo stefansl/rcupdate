d4425b8f3b2c97173de4aff9eb42642f9a431efd
Stefan Schulz-Lauterbach <ssl@clickpress.de>
2016-09-15 02:40:39 +0200

    add cd again and shorten

--- updateRoundcube.sh
+++ updateRoundcube.sh
@@ -11,13 +11,11 @@ USRGRP="clickpress.clickpress"
 VERSION=`curl -s https://api.github.com/repos/roundcube/roundcubemail/releases/latest | grep 'tag_name' | cut -d\" -f4`
 
 # GET PACKAGE
-wget https://github.com/roundcube/roundcubemail/releases/download/${VERSION}/roundcubemail-${VERSION}.tar.gz
-tar xvfz roundcubemail-${VERSION}.tar.gz
-cd roundcubemail-${VERSION}
+cd ${APPPATH} && wget https://github.com/roundcube/roundcubemail/releases/download/${VERSION}/roundcubemail-${VERSION}.tar.gz
+tar xvfz roundcubemail-${VERSION}.tar.gz && cd roundcubemail-${VERSION}
 
 # GET DEPS
-cp composer.json-dist composer.json
-composer install --no-dev
+cp composer.json-dist composer.json && composer install --no-dev
 
 # INSTALL TO ORIGINAL 
 ./bin/installto.sh $RCPATH
