0363112440c8f9a7527eb5fde2d10d2a34ca9c71
root <root@loft1244.serverloft.de>
2016-09-15 02:14:49 +0200

    first draft

---
+++ updateRoundcube.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Updatescript für Roundcube
+# Autor StefanSL
+
+APPPATH="/var/www/clickpress/apps/"
+RCPATH="/var/www/clickpress/apps/roundcube"
+USRGRP="clickpress.clickpress"
+
+#GET VERSIONNUMBER
+VERSION=`curl -s https://api.github.com/repos/roundcube/roundcubemail/releases/latest | grep 'tag_name' | cut -d\" -f4`
+
+wget https://github.com/roundcube/roundcubemail/releases/download/${VERSION}/roundcubemail-${VERSION}.tar.gz
+tar xvfz roundcubemail-${VERSION}.tar.gz
+cd roundcubemail-${VERSION}
+cp composer.json-dist composer.json
+composer install --no-dev
+./bin/installto.sh $RCPATH
+chown -R $USRGRP $RCPATH
+cd ..
+rm -R ${APPPATH}roundcubemail-${VERSION}
+rm -R ${APPPATH}roundcubemail-${VERSION}.tar.gz
