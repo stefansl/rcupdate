395c3581f47c3a9676d57e6b85ddcfe6610b8ebe
Stefan SL <ssl@clickpress.de>
2016-12-09 17:38:51 +0100

    Add version argument

--- updateRoundcube.sh
+++ updateRoundcube.sh
@@ -6,9 +6,24 @@ APPPATH="/var/www/clickpress/apps/"
 RCPATH="${APPPATH}roundcube"
 USRGRP="clickpress.clickpress"
 
+# Output help
+usage() {
+    echo "usage: ./updateRoundcube.sh [version]";
+    echo "-h            this help";
+    echo "-r version    choose a different version than the latest.";
+}
+
+while [[ "$#" > 1 ]]; do case $1 in
+    -r) VERSION="$2";;
+    *) break;;
+  esac; shift; shift
+done
+
 
 # GET VERSIONNUMBER
-VERSION=`curl -s https://api.github.com/repos/roundcube/roundcubemail/releases/latest | grep 'tag_name' | cut -d\" -f4`
+if [ -z "$VERSION" ]; then
+    VERSION=`curl -s https://api.github.com/repos/roundcube/roundcubemail/releases/latest | grep 'tag_name' | cut -d\" -f4`;
+fi
 
 # GET PACKAGE
 cd ${APPPATH} && wget https://github.com/roundcube/roundcubemail/releases/download/${VERSION}/roundcubemail-${VERSION}.tar.gz
